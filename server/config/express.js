9f51d0f6562a3002e6f1708625cad7300e40eaba
Nathan Kluth <nlkluth@gmail.com>
2014-05-02 16:48:48 -0600

    fixed express issues. added ui-view. working on better script inludes

--- server/config/express.js
+++ server/config/express.js
@@ -1,6 +1,29 @@
 'use strict';
+var express = require('express');
 
 module.exports = function (app, config) {
+
+  app.set('showStackError', true);
+  app.use(express.static(config.root + '/public'));
+
   app.set('views', config.root + '/server/views');
   app.set('view engine', 'jade');
+
+  app.use(function(err, req, res, next){
+    // treat as 404
+    if (~err.message.indexOf('not found')) {
+      return next();
+    }
+
+    // log it
+    console.error(err.stack);
+
+    // error page
+    res.status(500).render('500', { error: err.stack });
+  });
+
+  // assume 404 since no middleware responded
+  app.use(function(req, res) {
+    res.status(404).render('404', { url: req.originalUrl, error: 'Not found' });
+  });
 };
