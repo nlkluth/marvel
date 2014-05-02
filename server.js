ac0f4068b54133c11d1a53b0071d8e52f557251a
Nathan Kluth <nlkluth@gmail.com>
2014-05-02 15:03:21 -0600

    set basic server

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log('Express app started on port '+ port);
+
+// expose app
+exports = module.exports = app;
