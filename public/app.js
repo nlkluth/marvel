b048598dcfc22ae1ed557f0a43b3869f74a435fa
Nathan Kluth <nlkluth@gmail.com>
2014-05-02 14:43:26 -0600

    setting up app config

--- public/app.js
+++ public/app.js
@@ -1,2 +1,10 @@
+'use strict';
+
 angular.module('marvel', ['ui.router', 'restangular', 'marvel.characters'])
+
+.config(function ($locationProvider, RestangularProvider) {
+  RestangularProvider.setBaseUrl('/api');
+  $locationProvider.html5Mode(true).hashPrefix('!');
+})
+
 .constant('MARVEL', 'https://gateway.marvel.com/');
