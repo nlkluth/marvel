155e9d2c4b83cfa014f14eeaa66bd88fc48559af
Nathan Kluth <nlkluth@gmail.com>
2014-05-07 13:22:46 -0600

    update base url

--- server/config/env/all.js
+++ server/config/env/all.js
@@ -4,5 +4,5 @@ var rootPath = path.normalize(__dirname + '/../../..');
 module.exports = {
   root: rootPath,
   port: process.env.PORT || 3000,
-  marvel: 'https://gateway.marvel.com/'
+  marvel: 'https://gateway.marvel.com/v1'
 };
