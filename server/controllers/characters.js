f50aeb9f9c90b9732b5477b5de1e276a6e3ecd63
Nathan Kluth <nlkluth@gmail.com>
2014-05-07 13:24:08 -0600

    quick 'n dirty api test

--- server/controllers/characters.js
+++ server/controllers/characters.js
@@ -1,8 +1,29 @@
 'use strict';
 
+var request = require('request');
+var config = require('../config/config');
+var md5 = require('MD5');
 var debug = require('debug')('shim:characters');
 
 exports.list = function(req, res) {
-  debug('hit list');
-  res.send('hello world');
+  var timestamp = Date.now();
+  var publicKey = '74dea3270f83de408d2618a7d9cb001f';
+  var privateKey = '40103bf3248445ab3408ce4910473556b13c46c6';
+
+  var hash = md5(timestamp+privateKey+publicKey);
+  var url = config.marvel + '/public/characters?ts=' + timestamp + '&apikey=74dea3270f83de408d2618a7d9cb001f&hash=' + hash;
+
+  debug('requesting %s', url);
+
+  request(url, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      debug('no error, %s', body);
+      res.send(body);
+    }
+
+    console.log(response);
+    console.log(response.statusCode);
+    console.log(error);
+    console.log(body);
+  });
 };
