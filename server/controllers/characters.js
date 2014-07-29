d76121223ed96debf01aa283d55ca27e729138bd
Nathan Kluth <nlkluth@gmail.com>
2014-07-29 08:15:54 -0600

    remove hardcoded in favor of config & method

--- server/controllers/characters.js
+++ server/controllers/characters.js
@@ -2,17 +2,10 @@
 
 var request = require('request');
 var config = require('../config/config');
-var md5 = require('MD5');
 var debug = require('debug')('shim:characters');
 
 exports.list = function(req, res) {
-  var timestamp = Date.now();
-  var publicKey = '74dea3270f83de408d2618a7d9cb001f';
-  var privateKey = '40103bf3248445ab3408ce4910473556b13c46c6';
-
-  var hash = md5(timestamp+privateKey+publicKey);
-  var url = config.marvel + '/public/characters?ts=' + timestamp + '&apikey=74dea3270f83de408d2618a7d9cb001f&hash=' + hash;
-
+  var url = config.generateUrl('/public/characters');
   debug('requesting %s', url);
 
   request(url, function(error, response, body) {
