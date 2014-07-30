c21ed520f80dc2701ef01426568724621e3c5ec9
Nathan Kluth <nlkluth@gmail.com>
2014-07-30 08:37:14 -0600

    corrected method name

--- server/config/env/all.js
+++ server/config/env/all.js
@@ -9,7 +9,7 @@ module.exports = {
   marvel: 'https://gateway.marvel.com/v1',
   publicKey: '74dea3270f83de408d2618a7d9cb001f',
   privateKey: '40103bf3248445ab3408ce4910473556b13c46c6',
-  generateHash: function(url) {
+  generateUrl: function(url) {
     var timestamp = Date.now();
     var hash = md5(timestamp + this.privateKey + this.publicKey);
     return this.marvel + url + '?ts=' + timestamp + '&apikey=' + this.publicKey + '&hash=' + hash;
