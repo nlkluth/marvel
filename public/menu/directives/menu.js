cd2432892e9d858c62b8e6af7f4b4b62f250bce5
Nathan Kluth <nlkluth@gmail.com>
2014-08-10 00:15:02 -0600

    added menu height adjustments

--- public/menu/directives/menu.js
+++ public/menu/directives/menu.js
@@ -1,10 +1,22 @@
 'use strict';
 
 angular.module('marvel.menu')
-.directive('marvelMenu', function() {
+.directive('marvelMenu', function($window) {
   return {
     restrict: 'A',
     link: function(scope, element) {
+      var windowHeight = $window.innerHeight;
+      element.css('height', (windowHeight - 80) + 'px');
+      element.css('max-height', '710px');
+
+      var initializeWindowSize = function() {
+        element.css('height', ($window.innerHeight - 80) + 'px');
+      };
+
+      angular.element($window).bind('resize', function() {
+        initializeWindowSize();
+      });
+
       element.bind('mouseenter', function() {
         element.parent().find('.menu-item').addClass('contract');
         element.removeClass('contract');
