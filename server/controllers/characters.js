96de91b63adb6430faa1f7135c2024dbf7ec7d31
Nathan Kluth <nlkluth@gmail.com>
2014-08-10 00:15:08 -0600

    adjusting layout

--- server/controllers/characters.js
+++ server/controllers/characters.js
@@ -9,6 +9,10 @@ exports.list = function(req, res) {
   debug('requesting %s', url);
 
   request(url, function(error, response, body) {
+    if (error) {
+      res.send(error, 500);
+    }
+
     if (!error && response.statusCode === 200) {
       res.send(body);
     }
