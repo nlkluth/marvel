b7b4a8993fbdc6fdb3b2d2bee58ab82bed75c2d1
Nathan Kluth <nlkluth@gmail.com>
2014-08-13 08:15:15 -0600

    updating htmlmin path

--- Gruntfile.js
+++ Gruntfile.js
@@ -187,7 +187,7 @@ module.exports = function(grunt) {
         files: [{
           expand: true,
           cwd: '<%= yeoman.dist %>',
-          src: ['*.html', 'views/{,*/}*.html', 'views/{,*/}/{,*/}*.html'],
+          src: ['*.html', '**/*.html', 'views/{,*/}/{,*/}*.html'],
           dest: '<%= yeoman.dist %>'
         }]
       }
